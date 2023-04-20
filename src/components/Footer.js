5085e8674165c214c87b6b5f4a15be07ecd6e74b
John Seth Salazar <salazarjohnseth@gmail.com>
2023-04-20 10:06:16 +0800

    1st project commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Footer(){
+    return(
+        <div id="footer">
+            <small>Made by <a href="https://johnsethsalazar.netlify.app/" target="_blank" rel="noopener noreferrer">John Seth Salazar</a></small>
+        </div>
+    )
+}
